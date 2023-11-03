f76668290691720109f7c2b5e5fa3f0606d3a07d
yannqing <67121851@qq.com>
2023-11-03 10:21:19 +0800

    java11，springboot3.7.11

---
+++ src/main/java/com/jx/JxTeamUserApplication.java
@@ -0,0 +1,13 @@
+package com.jx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxTeamUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JxTeamUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jx/JxTeamUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JxTeamUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
